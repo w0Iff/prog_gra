c91511c4a4f91adf279c73b62d83a5afc2a46fd2
w0Iff <128382983+w0Iff@users.noreply.github.com>
2024-01-31 20:13:48 +0100

    update_2
    poprawki UI + sortowanie + troche kodu lab

---
+++ Kod_Labirynt/Labirynt_kod/Gracz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Gra_Labirynt
+{
+    class Gracz
+    {
+        public int Row { get; set; }
+
+        public int Column { get; set; }
+
+        public int SpawnRow { get; private set; }
+
+        public int SpawnColumn { get; private set; }
+
+        public Gracz(int startRow, int startCol)
+        {
+            Row = startRow;
+            Column = startCol;
+            SpawnRow = startRow;
+            SpawnColumn = startCol;
+        }
+    }
+}
---
+++ Kod_Labirynt/Labirynt_kod/Labirynt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Gra_Labirynt
+{
+    class Labirynt
+    {
+        private char[,] arch;
+
+       public int Rows { get; private set;}
+
+        public int Columns { get; private set;}
+
+        public Labirynt(char[,] arch)
+        {
+            this.arch = arch;
+            Rows = arch.GetLength(0);
+            Columns = arch.GetLength(1);
+        }
+        public bool IsMoveValid(int row, int col)
+        {
+            return row >= 0 && row < Rows &&
+                   col >= 0 && col < Columns &&
+                   arch[row, col] != '#';
+        }
+        public void PrintLabirynt(int playerRow, int playerCol)
+        {
+            Console.Clear();
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    if (i == playerRow && j == playerCol)
+                        Console.Write(arch[i, j]);
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }    
+        }
+    }
+
+}
